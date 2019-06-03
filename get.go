27bc72a34fceb9fa6a62fef1b7faf697f5c88130
Łukasz Magiera <magik6k@gmail.com>
2019-06-03 15:31:48 +0200

    setup gomod

--- get.go
+++ get.go
@@ -1,39 +1,37 @@
 package embeddedShell
 
 import (
-	"errors"
-	"fmt"
+	"context"
+	files "github.com/ipfs/go-ipfs-files"
+	"github.com/ipfs/interface-go-ipfs-core/path"
+	"io"
 
-	"github.com/ipfs/go-ipfs/core"
-	dag "github.com/ipfs/go-merkledag"
-	"github.com/ipfs/go-path"
-	uarchive "github.com/ipfs/go-unixfs/archive"
+	"github.com/ipfs/go-ipfs/core/coreapi"
 	tar "github.com/whyrusleeping/tar-utils"
 )
 
 // Cat resolves the ipfs path p and returns a reader for that data, if it exists and is availalbe
 func (s *Shell) Get(ref, outdir string) error {
-	ipfsPath, err := path.ParsePath(ref)
-	if err != nil {
-		return fmt.Errorf("get: could not parse %q: %s", ref, err)
-	}
-
-	nd, err := core.Resolve(s.ctx, s.node.Namesys, s.node.Resolver, ipfsPath)
-	if err != nil {
-		return fmt.Errorf("get: could not resolve %s: %s", ipfsPath, err)
-	}
-
-	pbnd, ok := nd.(*dag.ProtoNode)
-	if !ok {
-		return errors.New("could not cast Node to ProtoNode")
-	}
-
-	r, err := uarchive.DagArchive(s.ctx, pbnd, outdir, s.node.DAG, false, 0)
+	api, err := coreapi.NewCoreAPI(s.node)
 	if err != nil {
 		return err
 	}
 
-	ext := tar.Extractor{Path: outdir}
+	f, err := api.Unixfs().Get(context.Background(), path.New(ref))
+	if err != nil {
+		return err
+	}
 
-	return ext.Extract(r)
+	pr, pw := io.Pipe()
+	go func() {
+		tw, err := files.NewTarWriter(pw)
+		if err != nil {
+			_ = pw.CloseWithError(err)
+			return
+		}
+		_ = pw.CloseWithError(tw.WriteFile(f, outdir))
+	}()
+
+	ext := tar.Extractor{Path: outdir}
+	return ext.Extract(pr)
 }
---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/whyrusleeping/ipfs-embedded-shell
+
+go 1.12
+
+require (
+	github.com/ipfs/go-cid v0.0.2
+	github.com/ipfs/go-ipfs v0.4.21
+	github.com/ipfs/go-ipfs-api v0.0.1
+	github.com/ipfs/go-ipfs-chunker v0.0.1
+	github.com/ipfs/go-ipfs-config v0.0.3
+	github.com/ipfs/go-ipfs-files v0.0.3
+	github.com/ipfs/go-merkledag v0.0.6
+	github.com/ipfs/go-path v0.0.7
+	github.com/ipfs/go-unixfs v0.0.6
+	github.com/ipfs/interface-go-ipfs-core v0.0.8
+	github.com/whyrusleeping/tar-utils v0.0.0-20180509141711-8c6c8ba81d5c
+	golang.org/x/net v0.0.0-20190603091049-60506f45cf65
+	google.golang.org/appengine v1.4.0 // indirect
+)
